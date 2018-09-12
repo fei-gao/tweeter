a3c01c9a53206ed6fc06326a4e3ca2276f664ff1
Fei Gao <fgao1@ualberta.ca>
2018-09-12 09:40:05 -0600

    once submitting tweet, reload newly created tweet without refreshing page by refactoring submitForm function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,47 +11,46 @@ function createTweetElement(obj){
   const content = obj.content.text;
   const created_at = obj.created_at;
   
-  let $tweet = $('<article>').addClass("tweets");
-  
   // Header
+  const tweet = $('<article>').addClass("tweets");
   let header = $("<header>");
   let img = `<img src=${avatar} alt = 'image'>`;
-  let strong = `<strong id='name'> ${name} </strong>`;
-  let span = `<span id = 'handle'> ${handle} </span>`;
-  header.append(img).append(strong).append(span);
-  $tweet.append(header);
+  let personName = `<strong id='name'> ${name} </strong>`;
+  let personHandle = `<span id = 'handle'> ${handle} </span>`;
+  header.append(img).append(personName).append(personHandle);
+  tweet.append(header);
   
   // Message
   let message = $('<div>').addClass('tweet-message').text(`${content}`);
-  $tweet.append(message);
+  tweet.append(message);
   
   // Footer
   let footer = $("<footer>");
   let day = Math.floor((Date.now() - created_at) / 86400000);
   let date = `<span> ${day} days ago </span>`; 
-  let iconHtml = `<span class='icon'>  
-                    <i class='fab fa-font-awesome-flag'></i> 
-                    <i class='fas fa-retweet'></i>
-                    <i class='fas fa-heart'></i> 
-                  </span>`;
-  let icons = $('<span>').addClass('icon').html(iconHtml);
+  let icon = `<span class='icon'>  
+                <i class='fab fa-font-awesome-flag'></i> 
+                <i class='fas fa-retweet'></i>
+                <i class='fas fa-heart'></i> 
+              </span>`;
+  let icons = $('<span>').addClass('icon').html(icon);
   footer.append(date).append(icons);
-  $tweet.append(footer);
+  tweet.append(footer);
   
-  return $tweet;
+  return tweet;
 };
 
 function renderTweets(tweets) {
   tweets.forEach(function(tweet){
-    var $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
+    const tweetDOM = createTweetElement(tweet);
+    $('#tweets-container').prepend(tweetDOM);
   });
 }
 
 function submitForm(){
   $("form").on('submit',function(){
     event.preventDefault();
-    let isValid = validateForm();
+    const isValid = validateForm();
     if(isValid === true){
       console.log("form submmitted");
       $.ajax({
@@ -60,6 +59,8 @@ function submitForm(){
         data: $("form").serialize(),
         success: function(response){
           console.log('form submission success');
+          loadTweets();
+          $('textarea').val('');
         },
         error: function(){
           console.log("form submission fail");
@@ -68,42 +69,36 @@ function submitForm(){
     } else {
       console.log("form invalid and not submitted");
     }
-    
   })
 }
 
 function loadTweets(){
-$.ajax({
-  type:'GET',
-  url: '/tweets',
-}).then(function(json){
-  console.log("fetch success", json);
-  renderTweets(json);
-  })
+  $.ajax({
+    type:'GET',
+    url: '/tweets',
+  }).then(function(json){
+    console.log("fetch success", json);
+    renderTweets(json);
+    })
 };
 
 function validateForm(){
-    const str = $('textarea').val();
-    const length = str.length;
-    if( str === "" || length === 0){
-      alert("empty string");
-      return false;
-    } else if( length > 140){
-      alert("characters over 140");
-      return false;
-    } else {
-      return true;
-    }
+  const str = $('textarea').val();
+  const length = str.length;
+  if( str === ""){
+    alert("empty string");
+    return false;
+  } else if( length > 140){
+    alert("characters over 140");
+    return false;
+  } else {
+    return true;
+  }
 }
 
-// $(function() {
-// function addAllEvents(){
-loadTweets();
-submitForm();
+$('document').ready(function(){
 
-// };
+  loadTweets();
+  submitForm();
 
-
-// document.addEventListener("DOMContentLoaded", function(event){
-    // addAllEvents();
-// });
+});
