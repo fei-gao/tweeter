940bec7ebffecdb1b8e10a1815f822532e9d87fa
Fei Gao <fgao1@ualberta.ca>
2018-09-12 11:17:02 -0600

    refactor createTweetElement function to follow jquery conventions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,45 +5,40 @@
  */
 
 function createTweetElement(obj){
-  const avatar = obj.user.avatars.small;
-  const name = obj.user.name;
-  const handle = obj.user.handle;
-  const content = obj.content.text;
-  const created_at = obj.created_at;
+  const $tweet = $('<article>').addClass("tweets");
   
   // Header
-  const tweet = $('<article>').addClass("tweets");
-  let header = $("<header>");
-  let img = `<img src=${avatar} alt = 'image'>`;
-  let personName = `<strong id='name'> ${name} </strong>`;
-  let personHandle = `<span id = 'handle'> ${handle} </span>`;
-  header.append(img).append(personName).append(personHandle);
-  tweet.append(header);
+  let $header = $("<header>");
+  let $img = `<img src=${obj.user.avatars.small} alt = 'image'>`;
+  let $name = `<strong id='name'> ${obj.user.name} </strong>`;
+  let $handle = `<span id = 'handle'> ${obj.user.handle} </span>`;
+  $header.append($img).append($name).append($handle);
+  $tweet.append($header);
   
   // Message
-  let message = $('<div>').addClass('tweet-message').text(`${content}`);
-  tweet.append(message);
+  let $message = $('<div>').addClass('tweet-message').text(`${obj.content.text}`);
+  $tweet.append($message);
   
   // Footer
-  let footer = $("<footer>");
-  let day = Math.floor((Date.now() - created_at) / 86400000);
-  let date = `<span> ${day} days ago </span>`; 
+  let $footer = $("<footer>");
+  let day = Math.floor((Date.now() - `${obj.created_at}`) / 86400000);
+  let $date = `<span> ${day} days ago </span>`; 
   let icon = `<span class='icon'>  
                 <i class='fab fa-font-awesome-flag'></i> 
                 <i class='fas fa-retweet'></i>
                 <i class='fas fa-heart'></i> 
               </span>`;
-  let icons = $('<span>').addClass('icon').html(icon);
-  footer.append(date).append(icons);
-  tweet.append(footer);
+  let $icons = $('<span>').addClass('icon').html(icon);
+  $footer.append($date).append($icons);
+  $tweet.append($footer);
   
-  return tweet;
+  return $tweet;
 };
 
 function renderTweets(tweets) {
   tweets.forEach(function(tweet){
-    const tweetDOM = createTweetElement(tweet);
-    $('#tweets-container').prepend(tweetDOM);
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet);
   });
 }
 
